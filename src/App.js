b83f24a6437a139077ff3142f68f3d0c67868519
Andrei Fidelman <fidelmanlife@gmail.com>
2019-03-03 23:03:47 +0100

    Setup and Connect an Apollo Client to a React Application with Apollo Provider

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ApolloClient from 'apollo-boost'
+import gql from 'graphql-tag'
+import { ApolloProvider, ApolloConsumer } from 'react-apollo'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000'
+})
+
+// client
+//   .query({
+//     query: gql`
+//       {
+//         recipes {
+//           id
+//           title
+//         }
+//       }
+//     `
+//   })
+//   .then(result => console.log(result))
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ApolloProvider client={client}>
+        <h1>Hello</h1>
+        <ApolloConsumer>
+          {client => {
+            client
+              .query({
+                query: gql`
+                  {
+                    recipes {
+                      id
+                      title
+                    }
+                  }
+                `
+              })
+              .then(result => console.log(result))
+            return null
+          }}
+        </ApolloConsumer>
+      </ApolloProvider>
     );
   }
 }
