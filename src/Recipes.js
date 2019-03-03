c3aead9551d8340b5bb6e6e9fde53ff64c3a06ea
Andrei Fidelman <fidelmanlife@gmail.com>
2019-03-03 23:30:16 +0100

    Provide Dynamic Arguments in a Apollo Query Component with GraphQL Variables

--- src/Recipes.js
+++ src/Recipes.js
@@ -1,31 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 
 const recipesQuery = gql`
-  {
-    recipes {
+  query recipes($vegetarian: Boolean!){
+    recipes(vegetarian: $vegetarian) {
       id
       title
     }
   }
 `
 
-export default () => (
-  <Query
-    query={recipesQuery}
-  >
-    {({ data, loading, error }) => {
-      if (loading) return <p>Loading...</p>;
-      if (error) return <p>Error...</p>;
+export default () => {
+  const [vegetarian, setVegetarian] = useState(false)
 
-      return (
-        <ul>
-          {data.recipes.map(({ id, title }) => (
-            <li key={id}>{title}</li>
-          ))}
-        </ul>
-      );
-    }}
-  </Query>
-);
+  return (
+    <>
+      <label>
+        Vegetarian?
+        <input type="checkbox" checked={vegetarian} onChange={e => setVegetarian(e.target.checked)}/>
+      </label>
+      <Query
+        query={recipesQuery}
+        variables={{ vegetarian }}
+      >
+        {({ data, loading, error }) => {
+          if (loading) return <p>Loading...</p>;
+          if (error) return <p>Error...</p>;
+
+          return (
+            <ul>
+              {data.recipes.map(({ id, title }) => (
+                <li key={id}>{title}</li>
+              ))}
+            </ul>
+          );
+        }}
+      </Query>
+    </>
+  );
+}
