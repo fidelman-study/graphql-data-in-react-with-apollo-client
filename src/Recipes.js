e5e1e6902cefa7e78662e1dfb0ade9039cccaab0
Andrei Fidelman <fidelmanlife@gmail.com>
2019-03-04 09:56:08 +0100

    Manage Local State using Apollo by extending the GraphQL Schema on the Client

--- src/Recipes.js
+++ src/Recipes.js
@@ -1,28 +1,63 @@
-import React, { useState } from 'react'
-import { Query } from 'react-apollo';
-import recipesQuery from './recipesQuery'
+import React, { useState } from 'react';
+import { Query, Mutation } from 'react-apollo';
+import recipesQuery from './recipesQuery';
+import gql from 'graphql-tag';
+
+const updateRecipeStarredMutation = gql`
+  mutation updateRecipeStarred($id: ID!, $isStarred: Boolean!) {
+    updateRecipeStarred(id: $id, isStarred: $isStarred) @client
+  }
+`;
 
 export default () => {
-  const [vegetarian, setVegetarian] = useState(false)
+  const [vegetarian, setVegetarian] = useState(false);
 
   return (
     <>
       <label>
         Vegetarian?
-        <input type="checkbox" checked={vegetarian} onChange={e => setVegetarian(e.target.checked)}/>
+        <input
+          type="checkbox"
+          checked={vegetarian}
+          onChange={e => setVegetarian(e.target.checked)}
+        />
       </label>
-      <Query
-        query={recipesQuery}
-        variables={{ vegetarian }}
-      >
+      <Query query={recipesQuery} variables={{ vegetarian }}>
         {({ data, loading, error }) => {
           if (loading) return <p>Loading...</p>;
           if (error) return <p>Error...</p>;
 
           return (
             <ul>
-              {data.recipes.map(({ id, title }) => (
-                <li key={id}>{title}</li>
+              {data.recipes.map(({ id, title, isStarred }) => (
+                <li key={id}>
+                  {title}
+                  <Mutation
+                    mutation={updateRecipeStarredMutation}
+                    refetchQueries={[
+                      {
+                        query: recipesQuery,
+                        variables: { vegetarian: false }
+                      },
+                      {
+                        query: recipesQuery,
+                        variables: { vegetarian: true }
+                      }
+                    ]}
+                    awaitRefetchQueries
+                  >
+                    {(updateRecipeStarred, { loading }) => (
+                      <button onClick={() => updateRecipeStarred({
+                        variables: {
+                          id, isStarred: !isStarred
+                        }
+                      })}>
+                        {isStarred && '⭐️'}
+                        {loading && 'Loading...'}
+                      </button>
+                    )}
+                  </Mutation>
+                </li>
               ))}
             </ul>
           );
@@ -30,4 +65,4 @@ export default () => {
       </Query>
     </>
   );
-}
+};
